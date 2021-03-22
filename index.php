caefd29184b957c522ae6527838dda9ed106921a
Jehezkiel Eugene <jehezkiel.eugene@gmail.com>
2021-03-22 15:17:21 -0700

    sign up page is done; homepage rename to .php ; homepage detects if user is signed in. if user is not signed in it'll display sign up button

--- index.php
+++ index.php
@@ -15,7 +15,21 @@
 			<nav>
 				<a href="index.html">Home</a>
 				<a href="#">Search</a>
-				<a href="sign-in.html" id="sign-in-button">Sign In</a>
+				<?php 
+					session_start();
+
+					if (sizeof($_SESSION) == 0) {
+						$_SESSION["loggedIn"] = false;
+						$_SESSION["username"] = null;
+					}
+
+					if ($_SESSION["loggedIn"]) {
+						echo "<a href=\"sign-in.php\" id=\"sign-in-button\">Sign In</a>";
+					}
+					else {
+						echo "<a href=\"sign-up.php\" id=\"sign-in-button\">Sign Up</a>";
+					}
+				 ?>
 			</nav>
 
 		</header>
@@ -29,8 +43,6 @@
 				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
 			</div>
 		</div>
-		
 	</div>
-
 </body>
 </html>
