1248e111c427969a3b0a9a8e1f08a1c2cddc0136
CWYRenee <60048733+CWYRenee@users.noreply.github.com>
2021-04-14 22:10:25 -0700

    Updated admin php frontend- removed Bootstrap CSS

---
+++ Updated Admin PHP/adminEnableDisableUser.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href = "css/adminStyle.css">
+
+</head>
+<body>
+
+<div class="sidebar">
+  <a href="adminDashboard.php">Dashboard</a>
+  <a href="adminSearchUser.php">Search Users</a>
+  <a href="adminEndableDisableUser.php">Enable / Disable Users</a>
+  <class="active"><a href="#">Edit / Remove Posts</a>
+</div>
+
+<div class="content">
+  <h2>Enable or Disable User</h2>
+  <p>Enable or disable user here:</p>
+</div>
+</body>
+</html>
