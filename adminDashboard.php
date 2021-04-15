3bafb38de9ffaa9adf9fd7dd6024592555a839f0
CWYRenee <60048733+CWYRenee@users.noreply.github.com>
2021-04-15 14:27:56 -0700

    updated admin dashboard php without Bootstrap CSS

--- adminDashboard.php
+++ adminDashboard.php
@@ -1,15 +1,8 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-  <title>Admin Dashboard</title>
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <!-- Latest compiled and minified CSS -->
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-  <!-- jQuery library -->
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
-  <!-- Latest compiled JavaScript -->
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="stylesheet" href = "css/adminStyle.css">
 
   <?php 
       session_start();
@@ -36,62 +29,20 @@
       }
    ?>
 </head>
-<style>
-    /* Set height of the grid so .sidenav can be 100% (adjust as needed) */
-    .row.content {height: 550pt}
-    /* Set gray background color and 100% height */
-    .sidenav {
-        background-color: #f1f1f1;
-        height: 100%;
-    }
-    /* On small screens, set height to 'auto' for the grid */
-    @media screen and (max-width: 767px) {
-        .row.content {height: auto;} 
-    }
-</style>
 
 <body>
 
-<nav class="navbar navbar-inverse visible-xs">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-      <a class="navbar-brand" href="#">Dashboard</a>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li class="active"><a href="#">Dashboard</a></li>
-        <li><a href="adminSearchUser.php">User Account Management/a></li>
-        <li><a href="adminEditRemovePost.php">Edit / Remove Posts</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<div class="sidebar">
+  <class="active"><a href="#">Dashboard</a>
+  <a href="adminSearchUser.php">Search Users</a>
+  <a href="adminEnableDisableUser.php">Enable / Disable Users</a>
+  <a href="adminEditRemovePost.php">Edit / Remove Posts</a>
+</div>
 
-<div class="container-fluid">
-  <div class="row content">
-    <div class="col-sm-3 sidenav hidden-xs">
-      <h2><a href='index.php'>MyDiscussionForum</a></h2>
-      <ul class="nav nav-pills nav-stacked">
-        <li class="active"><a href="">Dashboard</a></li>
-        <li><a href="adminSearchUser.php">User Account Management</a></li>
-        <li><a href="adminEditRemovePost.php">Edit / Remove Posts</a></li>
-      </ul><br>
-    </div>
-    <br>
-    
-    <div class="col-sm-9">
-        <h3>At a Glance</h3>
-    </div>
-      <div class="row">
-        <div class="col-sm-3">
-          <div class="well">
-            <h4>Registered Users</h4>
-            <p>
+<div class="content">
+  <h2>Dashboard</h2>
+  <h4>Registered Users</h4>
+</div>
               <?php 
                   $query_registeredUsers =  "SELECT COUNT(Users.userId) AS REGISTERED_USERS FROM Users;";
                   $results = $PDO -> query($query_registeredUsers);
@@ -101,8 +52,6 @@
             </p> 
           </div>
         </div>
-        <div class="col-sm-3">
-          <div class="well">
             <h4>Posts</h4>
             <p>
               <?php 
@@ -114,8 +63,6 @@
             </p> 
           </div>
         </div>
-        <div class="col-sm-3">
-          <div class="well">
             <h4>Total Thread Views</h4>
             <p>
               <?php 
@@ -132,8 +79,6 @@
             </p> 
           </div>
         </div>
-        <div class="col-sm-3">
-          <div class="well">
             <h4>Total Thread Comments</h4>
             <p>
               <?php 
