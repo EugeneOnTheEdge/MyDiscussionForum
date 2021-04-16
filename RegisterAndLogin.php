f203be6d301cd42ed2226d3d14e85fbd0ed43929
bre48 <tszhei0408@gmail.com>
2021-04-15 17:57:34 -0700

    Design

--- RegisterAndLogin.php
+++ RegisterAndLogin.php
@@ -1,12 +1,26 @@
 <!DOCTYPE html>
 <html lang = "en">
     <head>
-        <title>Register or Log-in</title>
+        <title>MyDiscussionForum</title>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Poppins&display=swap" rel="stylesheet">
+        <link rel="stylesheet" type="text/css" href="css/style.css">
         <script type="text/javascript" src="js/account.js"></script>
     </head>
-    <header>
-        <h1>Register for an account or Log-in to your account</h1>
+    <header class="clearfix">
+        <h1 class="clearfix">MyDiscussionForum</h1>
+        
+        <nav>
+            <a href="index.php">Home</a>
+            <a href="#">Search</a>
+            <a href="new-thread.php">+ New Thread</a>
+            <?php require 'enforce-user-status.php' ?>
+        </nav>
+
+        <h2>Register for an account or Log-in to your account</h2>
+
     </header>
+       
     <body>
         <div class = "Registration_form" style = "width:50%; float:left">
             <form method="POST" action="register-user.php">
